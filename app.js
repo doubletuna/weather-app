46001caf4c56b02d7ea9bd6adc40e49fb5b048e1
Danny <dannyl@spectory.com>
2020-07-31 18:38:00 +0300

    finalized

--- app.js
+++ app.js
@@ -22,9 +22,14 @@ app.use(locationRoutes)
 app.use(weatherRoutes)
 app.use(express.static(publicPath))
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 app.listen(port, () => {
   console.log(`running at port ${port}`)
