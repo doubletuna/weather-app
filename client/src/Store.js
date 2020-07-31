db6e10f6c4b25234a888435f85115dce7b91a30d
Danny <dannyl@spectory.com>
2020-07-31 18:49:00 +0300

    wip

--- client/src/Store.js
+++ client/src/Store.js
@@ -1,10 +1,11 @@
 import {createStore} from 'redux'
-import {composeWithDevTools} from 'redux-devtools-extension'
+// import {composeWithDevTools} from 'redux-devtools-extension'
 import {applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 // import RootReducer from './reducers/RootReducer'
 import RootReducer from './redux/rootReducer'
 
-const Store = createStore(RootReducer, composeWithDevTools(applyMiddleware(thunk)));
+// const Store = createStore(RootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const Store = createStore(RootReducer);
 
 export default Store
